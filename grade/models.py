e93ba3b961dc89431e9b05c205b2039e4307e862
Framekumpuk <gunzritzicez_ts6@hotmail.com>
2017-04-13 20:14:04 +0000

    Create method in views.py that are index and operand.

--- grade/models.py
+++ grade/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Grade(models.Model):
     subject = models.CharField(max_length=200)
     score = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date published')
+    full = models.IntegerField(default=0)
--- grade/views.py
+++ grade/views.py
@@ -2,4 +2,23 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,"grade/index.html",'')
+
+def operand(request):
+    error_message = 0 
+    try:
+        subject = request.POST['subject']
+        score = request.POST['score']
+        full = request.POST['full']
+    except:
+        error_message = 1
+        subject = ""
+        score = ""
+        full = ""
+    else:
+            information = Grade(subject=subject, score=score, full=full)
+            information.save()
+    # user hits the Back button.
+    total = (score*100)/full
+    # Calculate balance of paylist.
+    return render(request,"grade/detail.html",{'payment_list':payment_list, 'total':total})
